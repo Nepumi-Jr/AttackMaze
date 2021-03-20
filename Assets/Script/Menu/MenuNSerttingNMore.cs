03563703f659fbf48d96efdc555a9f87e3f0f188
633040415-6 <76204479+633040415-6@users.noreply.github.com>
2021-03-20 22:02:20 +0700

    play, back , quit wait what quit????????

---
+++ Assets/Script/Menu/MenuNSerttingNMore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuNSerttingNMore : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject SettingPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doSetScreen("MainMenuPanel");
+    }
+
+    public void doSetScreen(string screen)
+    {
+        MainMenuPanel.SetActive(screen == "MainMenuPanel");
+        SettingPanel.SetActive(screen == "SettingPanel");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
