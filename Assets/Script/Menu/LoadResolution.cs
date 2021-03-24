838e50731efe539076fba02a33b61978751c4c8e
Nepumi-Jr <peerator@kkumail.com>
2021-03-24 23:09:29 +0700

    📝Load Resolution

---
+++ Assets/Script/Menu/LoadResolution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadResolution : MonoBehaviour
+{
+    Resolution[] resolutions;
+    Dropdown dropdownBruh;
+    List<Dropdown.OptionData> datas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        dropdownBruh = this.GetComponent<Dropdown>();
+        datas = new List<Dropdown.OptionData>();
+        foreach (Resolution res in resolutions)
+        {
+            datas.Add(new Dropdown.OptionData(res.ToString()));
+        }
+        dropdownBruh.ClearOptions();
+        dropdownBruh.AddOptions(datas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
